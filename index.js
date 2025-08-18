aa5488ccb9d2110e009c61b926c272213df7727a
k9web-dept <msg.from.dog@gmail.com>
2025-08-18 16:50:39 +0900

    Update index.js by J

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+
 app.post('/webhook', (req, res) => {
   console.log('LINE Webhook Received');
   console.log(JSON.stringify(req.body, null, 2));
